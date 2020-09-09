4eaaff8b8cdbc9d90b193128208d320c13d3b58d
Srikanth Katti <skatti@synamedia.com>
2020-09-09 17:03:42 +0530

    updated display message

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Hi, I am Srikanth Katti, I work at Synamedia.
+          Hi, I am Srikanth Katti, I work at Synamedia as a software engineer.
         </p>
       </div>
     );
